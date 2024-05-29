b09f4304aa769db1dc916cd0f8b726d4c0ee4911
Carlos Vasquez <carlos.vasquez@enapsys.com>
2024-05-28 18:23:05 -0600

    anonymous rest role

--- src/main/java/com/escod/kepler/security/rest/roles/AnonymousRestRole.java
+++ src/main/java/com/escod/kepler/security/rest/roles/AnonymousRestRole.java
@@ -1,5 +1,10 @@
 package com.escod.kepler.security.rest.roles;
 
+import com.escod.kepler.entity.activity.Activity;
+import io.jmix.security.model.EntityAttributePolicyAction;
+import io.jmix.security.model.EntityPolicyAction;
+import io.jmix.security.role.annotation.EntityAttributePolicy;
+import io.jmix.security.role.annotation.EntityPolicy;
 import io.jmix.security.role.annotation.ResourceRole;
 
 @ResourceRole(name = "AnonymousRestRole", code = AnonymousRestRole.CODE, scope = "API")
@@ -7,7 +12,10 @@ public interface AnonymousRestRole {
 
   String CODE = "anonymous-rest-role";
 
-  //  @EntityAttributePolicy(entityClass = CMSSite.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
-//  @EntityPolicy(entityClass = CMSSite.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.UPDATE})
-  void cmsSite();
+  @EntityAttributePolicy(entityClass = Activity.class,
+      attributes = "*",
+      action = EntityAttributePolicyAction.VIEW)
+  @EntityPolicy(entityClass = Activity.class,
+      actions = {EntityPolicyAction.READ, EntityPolicyAction.UPDATE})
+  void activity();
 }
