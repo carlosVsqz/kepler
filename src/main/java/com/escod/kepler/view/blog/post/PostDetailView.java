0d0dff50c0dfba7793d710fc1c39229185aab812
Carlos Vasquez <carlos.vasquez@enapsys.com>
2024-10-22 20:26:19 -0600

    [post] correcion en post para previsualizacion

--- src/main/java/com/escod/kepler/view/blog/post/PostDetailView.java
+++ src/main/java/com/escod/kepler/view/blog/post/PostDetailView.java
@@ -2,12 +2,16 @@ package com.escod.kepler.view.blog.post;
 
 import com.escod.kepler.entity.blog.Post;
 import com.escod.kepler.view.main.MainView;
-import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.component.richtexteditor.RichTextEditor;
+import io.jmix.flowui.component.tabsheet.JmixTabSheet;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "post/:id", layout = MainView.class)
 @ViewController("Post.detail")
@@ -15,20 +19,44 @@ import io.jmix.flowui.view.*;
 @EditedEntityContainer("postDc")
 public class PostDetailView extends StandardDetailView<Post> {
 
+  private static final String TAB_3 = "tab3";
+  private static final String INNER_HTML = "innerHTML";
+
+  @Autowired
+  protected UiComponents uiComponents;
+
   @ViewComponent
   private VerticalLayout content;
+
+  @ViewComponent
+  protected JmixTabSheet tabSheet;
+
+  @ViewComponent
+  private RichTextEditor htmlContentField;
 
   @Subscribe
   public void onInit(final InitEvent event) {
   }
 
-  @Subscribe("htmlContentField")
-  public void onHtmlContentFieldComponentValueChange(final AbstractField.ComponentValueChangeEvent<RichTextEditor, ?> event) {
-    String value = (String) event.getValue();
+  @Subscribe("tabSheet")
+  public void onTabSheetSelectedChange(final JmixTabSheet.SelectedChangeEvent event) {
 
-    content.removeAll();
-    Div span = new Div();
-    span.getElement().setProperty("innerHTML", value);
-    content.add(span);
+    Tab currentTab = event.getSelectedTab();
+    currentTab.getId().ifPresent(id -> {
+      if (id.equals(TAB_3)){
+        content.removeAll();
+        Component component = getComponent();
+        content.add(component);
+      }
+    });
+  }
+
+  private Component getComponent() {
+
+    String value = htmlContentField.getValue();
+    Div span = uiComponents.create(Div.class);
+    span.getElement().setProperty(INNER_HTML, value);
+
+    return span;
   }
 }
